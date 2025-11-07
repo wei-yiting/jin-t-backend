3b2bcc439078a4444d84118a913f42478e6090c2
Dong Wei (WYT) <wytdong@gmail.com>
2025-11-07 09:58:17 +0800

    feat: remove default value for model name and openai-api-key, add prompt

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
-import os
 from enum import Enum
 from typing import Annotated
 
-from dotenv import load_dotenv
 from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from openai import AsyncOpenAI
-
-load_dotenv()
 
 app = FastAPI()
 
-# TODO: make openai api key required
 # TODO: add audio file size validation (error handling)
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 
 class ModelName(str, Enum):
     GPT_4O_MINI_TRANSCRIBE = "gpt-4o-mini-transcribe"
     GPT_4O_TRANSCRIBE = "gpt-4o-transcribe"
-    GPT_4O_TRANSCRIBE_DIARIZE = "gpt-4o-transcribe-diarize"
 
 
 @app.post("/transcribe")
 async def convert_audio_to_text(
     audio_file: Annotated[UploadFile, File()],
-    model_name: Annotated[ModelName, Form()] = ModelName.GPT_4O_MINI_TRANSCRIBE,
-    openai_api_key: Annotated[str | None, Form()] = os.getenv("OPENAI_API_KEY"),
+    openai_api_key: Annotated[str, Form()],
+    model_name: Annotated[ModelName, Form()],
 ):
     client = AsyncOpenAI(api_key=openai_api_key)
     translation = await client.audio.transcriptions.create(
         model=model_name.value,
         file=(audio_file.filename, audio_file.file),
+        prompt="""語音中的中文語句中間可能夾雜英文，保留英文部分不需翻譯直接轉換為英文文字，中文部分用台灣使用的繁體中文呈現。
+        依據語氣和語句順暢度，使用合適的標點符號。""",
     )
 
     return {"transcription": translation.text}
